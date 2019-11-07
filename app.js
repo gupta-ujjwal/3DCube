5b74d1f44245e7bd680ddd0532ca1d94311276f4
Ujjwal Gupta <ujjwal.gupta.16csc@bml.edu.in>
2019-11-07 22:47:34 +0530

    Fixed the state Problem

--- app.js
+++ app.js
@@ -51,28 +51,28 @@ function line(context, p1,p2) {
 
 function create3DSquare(verticesUpdated) {
     ctx.clearRect(0,0,600,300);
-    // line(ctx, verticesUpdated[0],verticesUpdated[1]);
-    // line(ctx, verticesUpdated[1],verticesUpdated[2]);
-    // line(ctx, verticesUpdated[2],verticesUpdated[3]);
-    // line(ctx, verticesUpdated[3],verticesUpdated[0]);
-    // line(ctx, verticesUpdated[4],verticesUpdated[5]);
-    // line(ctx, verticesUpdated[5],verticesUpdated[6]);
-    // line(ctx, verticesUpdated[6],verticesUpdated[7]);
-    // line(ctx, verticesUpdated[7],verticesUpdated[4]);
-    // line(ctx, verticesUpdated[0],verticesUpdated[4]);
-    // line(ctx, verticesUpdated[1],verticesUpdated[5]);
-    // line(ctx, verticesUpdated[2],verticesUpdated[6]);
-    // line(ctx, verticesUpdated[3],verticesUpdated[7]);
-    for( var i = 0; i < faces.length; i++ ) {
-        var f = faces[i]
-        ctx.beginPath()
-        ctx.moveTo(verticesUpdated[f[0]].x,verticesUpdated[f[0]].y)
-        ctx.lineTo(verticesUpdated[f[1]].x,verticesUpdated[f[1]].y)
-        ctx.lineTo(verticesUpdated[f[2]].x,verticesUpdated[f[2]].y)
-        ctx.lineTo(verticesUpdated[f[3]].x,verticesUpdated[f[3]].y)
-        ctx.closePath()
-        ctx.stroke()
-    }
+    line(ctx, verticesUpdated[0],verticesUpdated[1]);
+    line(ctx, verticesUpdated[1],verticesUpdated[2]);
+    line(ctx, verticesUpdated[2],verticesUpdated[3]);
+    line(ctx, verticesUpdated[3],verticesUpdated[0]);
+    line(ctx, verticesUpdated[4],verticesUpdated[5]);
+    line(ctx, verticesUpdated[5],verticesUpdated[6]);
+    line(ctx, verticesUpdated[6],verticesUpdated[7]);
+    line(ctx, verticesUpdated[7],verticesUpdated[4]);
+    line(ctx, verticesUpdated[0],verticesUpdated[4]);
+    line(ctx, verticesUpdated[1],verticesUpdated[5]);
+    line(ctx, verticesUpdated[2],verticesUpdated[6]);
+    line(ctx, verticesUpdated[3],verticesUpdated[7]);
+    // for( var i = 0; i < faces.length; i++ ) {
+    //     var f = faces[i]
+    //     ctx.beginPath()
+    //     ctx.moveTo(verticesUpdated[f[0]].x,verticesUpdated[f[0]].y)
+    //     ctx.lineTo(verticesUpdated[f[1]].x,verticesUpdated[f[1]].y)
+    //     ctx.lineTo(verticesUpdated[f[2]].x,verticesUpdated[f[2]].y)
+    //     ctx.lineTo(verticesUpdated[f[3]].x,verticesUpdated[f[3]].y)
+    //     ctx.closePath()
+    //     ctx.stroke()
+    // }
 }
 
 function rotateCubeX() {
@@ -81,12 +81,12 @@ function rotateCubeX() {
     // Using Perspective Prohection
     for( var i = 0; i < vertices.length; i++ ) {
         var vertice = vertices[i];
-        var rotated = vertice.rotateX(angle);
+        var rotated = vertice.rotateX(1);
         var projected = rotated.project(400,200,100,3.5);
         t.push(projected);
+        vertices[i]=rotated;
     }
     
-    angle += 1;
     create3DSquare(t);
     // console.log(angle.x,angle.y,angle.z);
 }
@@ -97,12 +97,12 @@ function rotateCubeY() {
     // Using Perspective Prohection
     for( var i = 0; i < vertices.length; i++ ) {
         var vertice = vertices[i];
-        var rotated = vertice.rotateY(angle);
+        var rotated = vertice.rotateY(1);
         var projected = rotated.project(400,200,100,3.5);
         t.push(projected);
+        vertices[i] = rotated;
     }
     
-    angle += 1;
     create3DSquare(t);
     // console.log(angle.x,angle.y,angle.z);
 }
@@ -114,86 +114,60 @@ function rotateCubeZ() {
     // Using Perspective Prohection
     for( var i = 0; i < vertices.length; i++ ) {
         var vertice = vertices[i];
-        var rotated = vertice.rotateZ(angle);
+        var rotated = vertice.rotateZ(1);
         var projected = rotated.project(400,200,100,3.5);
         t.push(projected);
+        vertices[i] = rotated;
     }
     
     create3DSquare(t);
-    angle += 1;
     // console.log(angle.x,angle.y,angle.z);
 }
 
-function rotateAllD() {
-    var t = new Array();
-
-    for( var i = 0; i < vertices.length; i++ ) {
-        var v = vertices[i];
-        var r = v.rotateX(angle).rotateY(angle).rotateZ(angle);
-        var p = r.project(400,200,100,3.5);
-        t.push(p)
-    }
-
-    create3DSquare(t)
-}
 
 var vertices = [new Point3D(-1,1,-1), new Point3D(1,1,-1), new Point3D(1,-1,-1), new Point3D(-1,-1,-1),
     new Point3D(-1,1,1), new Point3D(1,1,1), new Point3D(1,-1,1), new Point3D(-1,-1,1)];
 
 var faces = [[0,1,2,3],[1,5,6,2],[5,4,7,6],[4,0,3,7],[0,4,5,1],[3,2,6,7]]
 
-var angle = 45;
+var angle = 1;
 var dacc=0.05,speed ;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var t = null;
 initCube();
 
 function rotateCubeXM() {
-    fr = 5
-    if (t!==null) {
-        clearTimeout(t);
-        t = null;
-        // console.log('Found');
-    }
-    for(var i = 0;i<150;i++,fr+=dacc) {
-        t = setTimeout(rotateCubeX,i*fr);
+    fr = 5;
+    for(var i = 0;i<200;i++,fr+=dacc) {
+        setTimeout(rotateCubeX,i*fr);
     }
 }
 
 function rotateCubeYM() {
-    if (t) {
-        clearTimeout(t);
-        t = null;
-    }
     fr = 5;
     for(let i = 0;i<200;i++,fr+=dacc) {
-        t = setTimeout(rotateCubeY,i*fr);
+        setTimeout(rotateCubeY,i*fr);
     }
 }
 
 function rotateCubeZM() {
-    if (t) {
-        clearTimeout(t);
-        t = null;
-    }
     fr = 5;
     for(let i = 0;i<200;i++,fr+=dacc) {
-        t = setTimeout(rotateCubeZ,i*fr);
+        setTimeout(rotateCubeZ,i*fr);
     }
 }
 
 function initCube() {
-   rotateAllD()
-    // var t = new Array();
+//    rotateAllD()
+    var t = new Array();
 
-    // // Using Perspective Prohection
-    // for( var i = 0; i < vertices.length; i++ ) {
-    //     var vertice = vertices[i];
-    //     var projected = vertice.project(400,200,128,3.5);
-    //     t.push(projected);
-    // }
-    
-    // create3DSquare(t);
+    for( var i = 0; i < vertices.length; i++ ) {
+        var vertice = vertices[i];
+        var rotated = vertice.rotateY(140);
+        var projected = rotated.project(400,200,100,3.5);
+        t.push(projected);
+        vertices[i] = rotated;
+    }
+    create3DSquare(t);
 }
